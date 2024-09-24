c2c8b53dea10e8b41f45113a8bdcd5b221087717
kvkkvk96 <kollivinod50@gmail.com>
2024-09-24 22:46:53 +0530

    practice

--- 6.arrays.sh
+++ 6.arrays.sh
@@ -6,4 +6,4 @@ echo "the first fruit is: ${FRUITS[0]}"
 echo "the second fruit is: ${FRUITS[1]}"
 echo "the third fruit is: ${FRUITS[2]}"
 
-echo "the total fruits are: ${FRUITS[$@]}"
+echo "the total fruits are: ${FRUITS[@]}"
