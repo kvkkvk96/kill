9e9698b92628c02c01a1c8f632d6c49a2108fe62
kvkkvk96 <kollivinod50@gmail.com>
2024-09-24 11:40:09 +0530

    practice

---
+++ 2.conversation.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+echo "Vinod:: hi Ravi"
+echo "Ravi:: hi Vinod, How are you?"
+echo "Vinod:: good, how you doing?"
+echo "Ravi:: doing good, do you looking for a job?"
+echo "Vinod:: yes ravi, I am looking for a job"
+echo "Ravi:: do you have knowledge in DevOps pilpelins?"
+echo "Vinod:: yes, I have knowledge in DevOps pipelines. also I have knowledge in AWS cloud."
+echo "Ravi:: good Vinod, there is an opportunity in my Organization. I will refer you"
+echo "Vinod:: Thank you so much Ravi. Thank you for your help." 
+echo "Ravi:: No thanks, be ready for an interview."
+
+
---
+++ 3.variables.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+PERSON1=Vinod
+PERSON2=Ravi
+
+echo "PERSON1:: hi PERSON2"
+echo "PERSON2:: hi PERSON1, How are you?"
+echo "PERSON1:: good, how you doing?"
+echo "PERSON2:: doing good, do you looking for a job?"
+echo "PERSON1:: yes PERSON2, I am looking for a job"
+echo "PERSON2:: do you have knowledge in DevOps pilpelins?"
+echo "PERSON1:: yes, I have knowledge in DevOps pipelines. also I have knowledge in AWS cloud."
+echo "PERSON2:: good PERSON1, there is an opportunity in my Organization. I will refer you"
+echo "PERSON1:: Thank you so much PERSON2. Thank you for your help." 
+echo "PERSON2:: No thanks, be ready for an interview."
