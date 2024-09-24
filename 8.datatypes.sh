317f8921bbbe9f4a804fb4190b64870199c11093
kvkkvk96 <kollivinod50@gmail.com>
2024-09-24 23:22:30 +0530

    practice

--- 8.datatypes.sh
+++ 8.datatypes.sh
@@ -8,4 +8,4 @@ DIF=$(($NUMBER1/$NUMBER2))
 TOTAL=$(($SUM+$DIF))
 
 echo "the total of the given two numbers are: $SUM"
-echo "the total of $SUM and $DIF is: $TOTAL"
+echo "the total of the $SUM and the total of the $DIF of the given two numbers is: $TOTAL"
