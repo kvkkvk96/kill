85593373008cca7078d371c9512f6a297eff374b
kvkkvk96 <kollivinod50@gmail.com>
2024-09-24 22:45:46 +0530

    practice

--- 6.arrays.sh
+++ 6.arrays.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-FRUITS={ ["kiwi" "apple" "mango"]}
+FRUITS=("kiwi" "apple" "mango")
 
-echo "the first fruit is: ${FRUITS[$0]}"
-echo "the first fruit is: ${FRUITS[$1]}"
-echo "the first fruit is: ${FRUITS[$2]}"
-echo "the first fruit is: ${FRUITS[$1]}"
+echo "the first fruit is: ${FRUITS[0]}"
+echo "the second fruit is: ${FRUITS[1]}"
+echo "the third fruit is: ${FRUITS[2]}"
+
 echo "the total fruits are: ${FRUITS[$@]}"
