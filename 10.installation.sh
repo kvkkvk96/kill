28ef7adf8dee5c58d57bf37b4c7fb8f48a238b5c
kvkkvk96 <kollivinod50@gmail.com>
2024-10-02 13:46:48 +0530

    practice

--- 10.installation.sh
+++ 10.installation.sh
@@ -4,27 +4,25 @@ USERID=$(id -u)
 #make sure that the sript executing with root previleages.
 
 if [$USERID -ne 0]
-then {
+then 
      echo "please execute the script with root previleages"
      exit1
-}
+fi
 
 dnf list installed git
 if [$? -ne 0]
-then {
+then 
       echo "git is not installed... please install it"
       dnf install git
     if [$? -ne 0]
-then {
+then 
     echo "git installation failed....please check"
     exit1
-}
-    else {
+
+    else 
         echo "git installation success"
-    }
     fi
-}
-else {
+
+else 
     echo "git is already installed"
-}
 fi
